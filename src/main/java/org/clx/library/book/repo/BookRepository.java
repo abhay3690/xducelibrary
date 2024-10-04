4fa6c650ae893128e1bf3951d925be120d061cb4
Abhay S <abhaysuthar7777@gmail.com>
2024-10-04 22:09:00 +0530

    add extra spaces

--- src/main/java/org/clx/library/book/repo/BookRepository.java
+++ src/main/java/org/clx/library/book/repo/BookRepository.java
@@ -8,6 +8,8 @@ import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book,Long> {
+
     List<Book> findByTitleContaining(String title);
+    
     List<Book> findByAuthorContaining(String author);
 }
